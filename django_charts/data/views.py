14c04e25d05fbe0666cf1aecef62f4987bb546c1
Buttee-bit <63400112+Buttee-bit@users.noreply.github.com>
2022-06-19 23:24:46 +0300

    all_test

--- django_charts/data/views.py
+++ django_charts/data/views.py
@@ -2,11 +2,35 @@ from multiprocessing import context
 import pandas as pd
 import json
 from django.http import JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib import messages
+from django.core.files.storage import FileSystemStorage
+import os
+
+# Create your views here
 
 
 def index(request):
-    context = {
-        
-    }
-    return render(request,'dashboard/index.html',context)
+    if request.method =="POST":
+        uploaded_file = request.FILES['document']
+
+        if uploaded_file.name.endswith('.csv'):
+            #save csv file in media folder 
+            savefile = FileSystemStorage()
+
+            name = savefile.save(uploaded_file.name, uploaded_file) # this is a name file
+            
+            #know where to save file
+            d = os.getcwd() # Current directory of the project
+            file_directiry = d+'\media\\'+name
+            return redirect(results)
+    else:
+        messages.warning(request, 'File was not uploaded. Please use csv file extension !')
+
+    return render(request,'dashboard/index.html')
+
+def readfile(filename):
+    pass
+            
+def results(request):
+    return render(request,'results.html')
--- django_charts/django_charts/urls.py
+++ django_charts/django_charts/urls.py
@@ -18,5 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('data.urls'))    
+    path('', include('data.urls')),
+    path('results', include('data.urls')),    
+    
 ]
